86b36ac37f4a0459051207365da3688cd08cc09a
person50605 <124418091+person50605@users.noreply.github.com>
2026-09-10 09:54:50 -0700

    Annotate TriplicateSlash.java

--- plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/spells/falsespirit/TriplicateSlash.java
+++ plugins/paper/src/main/java/com/playmonumenta/plugins/bosses/spells/falsespirit/TriplicateSlash.java
@@ -1,3 +1,36 @@
+/**
+6 sec cd
+
+6 60 degree wedges, with every other filled
+
+Alternated filledness with each slash
+
+7 block radius?
+
+3 slashes over 4.5 seconds
+
+only castable if a player is within 10 blocks
+
+Can't bypass iframes and causes knockback
+
+
+
+
+
+**/
+
+
+
+
+
+
+
+
+
+
+
+
+
 package com.playmonumenta.plugins.bosses.spells.falsespirit;
 
 import com.playmonumenta.plugins.bosses.bosses.FalseSpirit;
@@ -29,7 +62,7 @@ public class TriplicateSlash extends Spell {
 	private final LivingEntity mBoss;
 
 	//direction is 0 or 180
-	private int mDirection = 0;
+	private int mDirection = 0; // direction of the slash, not where the slashes are pointing. Toggled with each slash
 
 	public TriplicateSlash(Plugin plugin, LivingEntity boss) {
 		mPlugin = plugin;
@@ -39,7 +72,7 @@ public class TriplicateSlash extends Spell {
 	@Override
 	public void run() {
 		World world = mBoss.getWorld();
-		mDirection = 0;
+		mDirection = 0; //always goes one way first
 
 		Vector dir = mBoss.getLocation().getDirection();
 
@@ -47,23 +80,23 @@ public class TriplicateSlash extends Spell {
 			int mTicks = 0;
 
 			@Override
-			public void run() {
+			public void run() { // runs every 2 ticks
 				Location loc = mBoss.getLocation();
 				loc.setDirection(dir);
 
-				if (mTicks > 0 && mTicks % 30 == 0) {
+				if (mTicks > 0 && mTicks % 30 == 0) { //every 1.5 sec after the attack starts
 					Vector vec;
 					List<BoundingBox> boxes = new ArrayList<>();
 
 					world.playSound(loc, Sound.ENTITY_PLAYER_ATTACK_SWEEP, SoundCategory.HOSTILE, 3, 0.5f);
 
 					//Final particle show
-					for (double r = 0; r < 7; r++) {
-						for (int dir = mDirection; dir <= 270 + mDirection; dir += 120) {
-							for (double degree = 60; degree < 120; degree += 5) {
+					for (double r = 0; r < 7; r++) { // runs 7 times, showing a radius of 7 blocks
+						for (int dir = mDirection; dir <= 270 + mDirection; dir += 120) { // 120 degree sized slices. Runs a total of 3 times
+							for (double degree = 60; degree < 120; degree += 5) { // split the 120 degree slices into 2 60 degree slices
 								double radian1 = Math.toRadians(degree);
-								vec = new Vector(FastUtils.cos(radian1) * r, 0, FastUtils.sin(radian1) * r);
-								vec = VectorUtils.rotateYAxis(vec, loc.getYaw() + dir);
+								vec = new Vector(FastUtils.cos(radian1) * r, 0, FastUtils.sin(radian1) * r); //ring gets bigger
+								vec = VectorUtils.rotateYAxis(vec, loc.getYaw() + dir); //rotate the vec so that the slash attack is in the same direction as the Spirit
 
 								Location l = loc.clone().add(vec);
 								//1.5 -> 15
@@ -80,7 +113,7 @@ public class TriplicateSlash extends Spell {
 						DamageUtils.damage(mBoss, player, DamageType.MAGIC, 30, null, false, true, "Triplicate Slash");
 					}
 
-					if (mTicks >= 90) {
+					if (mTicks >= 90) { //if more than 4.5 sec, do the last one at 0 deg. Should be a total of 3 slashes.
 						mDirection = 0;
 						this.cancel();
 					} else if (mDirection == 0) {
@@ -88,7 +121,7 @@ public class TriplicateSlash extends Spell {
 					} else {
 						mDirection = 0;
 					}
-				} else {
+				} else { // make particles to show the hitbox
 					world.playSound(mBoss.getLocation(), Sound.UI_TOAST_IN, SoundCategory.HOSTILE, 2, 2f);
 
 					for (int dir = mDirection; dir <= 270 + mDirection; dir += 120) {
